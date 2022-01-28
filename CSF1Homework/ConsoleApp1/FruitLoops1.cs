27cb9364ab70e6cecdc7c006518fbbe7704bb644
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-01-28 16:08:22 -0600

    created loops that contained arrarys for fruit loops 1 and 2

---
+++ CSF1Homework/ConsoleApp1/FruitLoops1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+
+{
+    class FruitLoops1
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "FruitLoops1";
+
+            string[] fruits = { "apple", "bannana", "strawberry", "cherry", "grape", "orange" };
+
+            
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+
+        }
+    }
+}
---
+++ CSF1Homework/ConsoleApp1/FruitLoops2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class FruitLoops2
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Fruit Loops 2";
+
+            string[] fruits = { "apple\n.", "bannana\n..", "strawberry\n...", "cherry\n....", "grape\n.....", "orange\n......" };
+
+
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine(fruits[i]);
+                
+            }
+
+        }//end Main()
+    }//end class
+}//end namespace
