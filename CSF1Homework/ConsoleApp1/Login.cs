071c4a7475afe9fde8fa07f8765e79d24c4e86d9
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-01-29 11:58:17 -0600

    created a loop that will lock you out of logging in if username is incorrect 3 times by using an if tree

---
+++ CSF1Homework/ConsoleApp1/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Login";
+
+            for (int i = 0; i < 3; i++)
+            {
+            Console.Write("Login\nusername: ");
+            string userName = Console.ReadLine().ToLower();
+  
+            if (userName == "correct username")
+            {
+                Console.WriteLine("You have been granted access.");
+                    break;//breaks you out of loop
+            }
+
+            else if ( userName != "correct username")
+            {
+                Console.WriteLine("You have been denied access.");
+            if (i == 2)
+            {
+                Console.WriteLine("You have been locked out.");
+
+            }
+            }
+            
+            }
+            
+        }//end Main()
+    }//end class
+}//end namespace
